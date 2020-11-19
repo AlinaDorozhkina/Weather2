fa03b45d9eb74c426c54d8c15e02afc7de0972c5
Alina_D <alinadorozhkina91@mail.ru>
2020-11-19 12:08:40 +0300

    передеала приложение, перевела во фрагменты. Не доделала land ориентацию еще

--- app/src/main/java/com/example/weatherapp/City.java
+++ app/src/main/java/com/example/weatherapp/City.java
@@ -1,4 +1,45 @@
 package com.example.weatherapp;
 
-public class City {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class City implements Parcelable {
+    private String name;
+    private int temperature;
+
+    public City (String name, int temperature){
+        this.name=name;
+        this.temperature=temperature;
+
+    }
+    protected City(Parcel in) {
+        name =in.readString();
+        temperature=in.readInt();
+        
+
+    }
+
+
+    public static final Creator<City> CREATOR = new Creator<City>() {
+        @Override
+        public City createFromParcel(Parcel in) {
+            return new City(in);
+        }
+
+        @Override
+        public City[] newArray(int size) {
+            return new City[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(temperature);
+    }
 }
--- app/src/main/java/com/example/weatherapp/WeatherDescription.java
+++ app/src/main/java/com/example/weatherapp/WeatherDescription.java
@@ -1,14 +1,21 @@
 package com.example.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.Key;
 
 public class WeatherDescription extends AppCompatActivity {
     private static final String TAG = WeatherDescription.class.getSimpleName();
@@ -18,10 +25,13 @@ public class WeatherDescription extends AppCompatActivity {
     private boolean flag = false;
     private String city;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_description);
+
         textViewTemperature=findViewById(R.id.textViewTemperature);
         textViewCity = findViewById(R.id.textViewCity);
         favourites_button = findViewById(R.id.favourites_button);
@@ -32,11 +42,34 @@ public class WeatherDescription extends AppCompatActivity {
             textViewCity.setText(city);
             textViewTemperature.setText(String.format("%s °", showRandomValue()));
         }
+        boolean isPressureTrue=getIntent().getBooleanExtra(Keys.PRESSURE, false);
+        boolean isWindSpeedTrue=getIntent().getBooleanExtra(Keys.WIND_SPEED, false);
+        boolean isMoistureTrue=getIntent().getBooleanExtra(Keys.MOISTURE, false);
+
+        if (isPressureTrue ||isWindSpeedTrue||isMoistureTrue ){
+            FragmentManager manager=getSupportFragmentManager();
+            FragmentTransaction transaction=manager.beginTransaction();
+            ExtraDataFragment fragment=new ExtraDataFragment();
+            transaction.add(R.id.frame_for_extra_layout, fragment);
+            Bundle bundle=new Bundle();
+            if (isPressureTrue){
+                bundle.putBoolean(Keys.PRESSURE, true);
+            }
+            if (isWindSpeedTrue){
+                bundle.putBoolean(Keys.WIND_SPEED, true);
+            }
+            if (isMoistureTrue){
+                bundle.putBoolean(Keys.MOISTURE, true);
+            }
+            fragment.setArguments(bundle);
+            transaction.commit();
+        }
     }
 
+
+
     private  String showRandomValue() {
-        int random = (int) (Math.random() * 30);
-        return String.valueOf(random);
+        return String.valueOf((int) (Math.random() * 30));
     }
 
     public void addToFavourites(View view) {
